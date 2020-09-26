dbcd237cdd5e50b1ed2c5d3614c9649e5d9f1385
Timothy Shields <Timothy.Shields@twosigma.com>
2020-09-26 12:27:51 -0500

    Add tests for uberjob.render and uberjob.progress.html_progress

---
+++ tests/test_progress.py
@@ -0,0 +1,46 @@
+#
+# Copyright 2020 Two Sigma Open Source, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import tempfile
+
+import pytest
+
+import uberjob
+
+
+def add(x, y):
+    return x + y
+
+
+def fail():
+    raise Exception("Something failed.")
+
+
+def test_html_progress():
+    plan = uberjob.Plan()
+    x = plan.call(add, 2, 3)
+    with tempfile.TemporaryDirectory() as temp:
+        path = os.path.join(temp, "uberjob.html")
+        uberjob.run(plan, output=x, progress=uberjob.progress.html_progress(path))
+
+
+def test_html_progress_with_exception():
+    plan = uberjob.Plan()
+    x = plan.call(fail)
+    with pytest.raises(uberjob.CallError):
+        with tempfile.TemporaryDirectory() as temp:
+            path = os.path.join(temp, "uberjob.html")
+            uberjob.run(plan, output=x, progress=uberjob.progress.html_progress(path))
